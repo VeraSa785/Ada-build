5e6b2b4449d028bcec7705344f95158a77bd7174
VeraSa785 <54861327+VeraSa785@users.noreply.github.com>
2021-03-11 15:56:32 -0800

    Update Project_Rock_Paper_Scissors.py

--- Project_Rock_Paper_Scissors.py
+++ Project_Rock_Paper_Scissors.py
@@ -24,7 +24,10 @@ elif player2 == 'scissors':
         print('Player 2 won!')
     else:
         print('Player 1 won!')
+              
+else: print('It\'s a tie!')
         
+# __________________________________________
 # 2nd solution (3.5 method)
 
 player1 = 'rock'
@@ -50,14 +53,22 @@ elif player1 == 'scissors' or player2 == 'scissors':
 else:
     print('It\'s a tie!')
         
+# _________________________________________________
 # 3rd solution (3.6 method)
 
 player1 = 'scissors'
 player2 = 'paper'
 
-if player1 == 'paper' and player2 != 'scissors':
-    print('Player 1 won!')
-    else:
-        print('It\'s a tie!')
-if player1 == 'paper' and player2 == 'scissors':
-    print('Player 1 won!')
+
+if player1 == player2:
+    print('It\'s a tie!')
+elif player1 == 'paper' and player2 == 'rock':
+       print('Player 1 won!')
+elif player1 == 'rock' and player2 == 'scissors':
+       print('Player 1 won!')
+elif player1 == 'scissors' and player2 == 'paper':
+       print('Player 1 won!')
+else:
+    print('Player 2 won!')
+     
+          
