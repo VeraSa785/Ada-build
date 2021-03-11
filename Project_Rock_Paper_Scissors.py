80dbf2a21a0a6877cbf4b0f549d6ac9628dd6f01
VeraSa785 <54861327+VeraSa785@users.noreply.github.com>
2021-03-11 12:59:30 -0800

    Update Project_Rock_Paper_Scissors.py

--- Project_Rock_Paper_Scissors.py
+++ Project_Rock_Paper_Scissors.py
@@ -1,39 +1,13 @@
 
-# 1st solution
-
-player1 = 'rock'
-player2 = 'paper' 
-
-if player1 == 'paper' or player2 == 'paper':
-    if player2 == 'rock':
-        print('Player 1 won!')
-    elif player2 == 'scissors':
-        print('Player 2 won!')
-    else:
-        print('It\'s a tie!')
-        
-if player1 == 'rock' or player2 == 'rock':
-    if player2 == 'paper':
-        print('Player 2 won!')
-    elif player2 == 'scissors':
-        print('Player 1 won!')
-    else:
-        print('It\'s a tie!')
-        
- if player1 == 'scissors' or player2 == 'scissors':
-    if player2 == 'paper':
-        print('Player 1 won!')
-    elif player2 == 'rock':
-        print('Player 2 won!')
-    else:
-        print('It\'s a tie!')
-        
-# 2nd solution
+# 1st solution (3.4 method)
        
 player1 = 'rock'
 player2 = 'paper'
 
-if player2 == 'paper':
+if player1 == player2:
+    print('It\'s a tie!')
+
+elif player2 == 'paper':
     if player1 == 'rock':
         print('Player 2 won!')
     else:
@@ -50,5 +24,40 @@ elif player2 == 'scissors':
         print('Player 2 won!')
     else:
         print('Player 1 won!')
+        
+# 2nd solution (3.5 method)
+
+player1 = 'rock'
+player2 = 'paper' 
+
+if player1 == 'paper' or player2 == 'paper':
+    if player2 == 'rock':
+        print('Player 1 won!')
+    elif player2 == 'scissors':
+        print('Player 2 won!')
+        
+elif player1 == 'rock' or player2 == 'rock':
+    if player2 == 'paper':
+        print('Player 2 won!')
+    elif player2 == 'scissors':
+        print('Player 1 won!')
+        
+elif player1 == 'scissors' or player2 == 'scissors':
+    if player2 == 'paper':
+        print('Player 1 won!')
+    elif player2 == 'rock':
+        print('Player 2 won!')
 else:
     print('It\'s a tie!')
+        
+# 3rd solution (3.6 method)
+
+player1 = 'scissors'
+player2 = 'paper'
+
+if player1 == 'paper' and player2 != 'scissors':
+    print('Player 1 won!')
+    else:
+        print('It\'s a tie!')
+if player1 == 'paper' and player2 == 'scissors':
+    print('Player 1 won!')
